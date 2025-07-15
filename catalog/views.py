061e8838bb3a9cb3173da6bf8a0e17eb7af11206
sirgecko84 <khanhtruong2404@gmail.com>
2025-07-15 11:23:17 +0700

    Django Tutorial Part5: Creating out homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+
+    num_instances_available = BookInstance.objects.filter(status=BookInstance.LoanStatus.AVAILABLE).count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
