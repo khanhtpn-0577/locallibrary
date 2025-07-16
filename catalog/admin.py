0353018cfc23bc14a816ff876c791ff11d1f0bb4
sirgecko84 <khanhtruong2404@gmail.com>
2025-07-16 17:22:05 +0700

    Django Tutorial Part6: Generic list and detail views

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,18 +4,23 @@ from .models import Author, Genre, Book, BookInstance
 
 admin.site.register(Genre)
 
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name',  ('date_of_birth', 'date_of_death')]
+
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
